2df369d4c361b1098276e0535ab5546a102b4202
edwinsharon <edwinachayan@gmail.com>
2024-06-12 18:00:15 +0530

    seller commit

--- ecommerce project/ecompro/ecomapp/urls.py
+++ ecommerce project/ecompro/ecomapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name="index"),
+    path('sellerlogin/',views.sellersignup,name="sellersignup"),
+    path('seller/',views.sellerlogin,name="sellerlogin"),
+    path('sellerindex/',views.sellerindex,name="sellerindex"),
+    path('logout/',views.logout,name="logout")
 ]
--- ecommerce project/ecompro/ecomapp/views.py
+++ ecommerce project/ecompro/ecomapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.models import User
 
@@ -11,6 +11,22 @@ def sellersignup(request):
         password=request.POST.get('password')
         User.objects.create_user(username=username,password=password)
         return redirect('sellerlogin')
-    return render(request,"sellerlogin.html")
+    return render(request,"sellercreate.html")
 
-def 
+def sellerlogin(request):
+    if 'username' in request.session:
+        return redirect('sellerindex')
+    if request.POST:
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            request.session['username']=username
+            return redirect("sellerindex")
+    return render(request,"sellerlogin.html")    
+def sellerindex(request):
+    return render(request,"sellerindex.html")    
+def logout(request):
+        logout(request)
+        return redirect('sellerlogin')
