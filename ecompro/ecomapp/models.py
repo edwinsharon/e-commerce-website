85a2fee46af1cb1f0e23860a3820a6d948d38822
edwinsharon <edwinachayan@gmail.com>
2024-06-14 13:20:49 +0530

    add product

---
+++ ecompro/ecomapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class product(models.Model):
+    productname=models.CharField(maxlength=100)
+    prize=models.IntegerField()
+    offer=models.CharField(max_length=50)
+    speed=models.CharField(max_length=50)
+    color=models.CharField(max_length=50)
+    description=models.CharField(max_length=300)
+    category=models.CharField(max_length=50)
+    image=models.ImageField(upload_to='static/images/product/')
+    
---
+++ ecompro/ecomapp/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .models import *
+
+# Create your views here.
+def index(request):
+    return render(request,"index.html")
+def sellersignup(request):
+    if request.POST:
+        email=request.POST.get('email')
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        confirmpassword=request.POST.get('confirmpassword')
+        
+        if not username or not email or not password or not confirmpassword:
+            messages.error(request,'all fields are required.')
+
+        elif confirmpassword != password:
+            messages.error(request,"password doesnot match")
+           
+        elif User.objects.filter(email=email).exists():
+            messages.error(request,"email already exist")
+           
+        elif User.objects.filter(username=username).exists():
+            messages.error(request,"username already exist")
+
+        else:
+           
+            user = User.objects.create_user(username=username, email=email, password=password)    
+            user.is_staff=True
+            user.save()
+            messages.success(request,"account created successfully")
+            return render(request, "sellercreate.html")
+    return render(request,"sellercreate.html")    
+           
+
+def sellerlogin(request):
+    if 'username' in request.session:
+        return redirect('sellerindex')
+    if request.POST:
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            request.session['username']=username
+            return redirect("sellerindex")
+    return render(request,"sellerlogin.html")    
+def sellerindex(request):
+    return render(request,"sellerindex.html")    
+def logoutseller(request):
+    logout(request)
+    return redirect('sellerlogin')
+def additem(request):
+    if request.POST:
+        productname=request.POST.get("productname")
+        prize=request.POST.get("prize")
+        offer=request.POST.get("offer")
+        speed=request.POST.get("speed")
+        color=request.POST.get("color")
+        description=request.POST.get("description")
+        category=request.POST.get("category")
+        
+
+    return render (request,"addpro.html")
+
